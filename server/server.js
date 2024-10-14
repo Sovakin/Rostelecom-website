d56ef506f7ffb8b6606b71ad5db0b7f4b4d49d43
JAVE232323 <a.tyulmenev@yandex.ru>
2024-10-15 00:52:39 +0300

    add main get method for app

--- server/server.js
+++ server/server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.json({message: "123"})
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
