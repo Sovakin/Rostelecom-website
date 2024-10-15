0434c148a2e717e059e3186a076733b78d8af675
JAVE <a.tyulmenev@yandex.ru>
2024-10-15 10:59:49 +0300

    add moduls for GTts made LongPulling tech

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require ('express');
 const cors = require('cors');
+const events = require('events');
 const PORT = 5000;
+const OpenAI = require('openai');
+const openai = new OpenAI({apiKey:'qweqweqw'})
+
+const emitter = new events.EventEmitter();
 
 const app = express();
 
@@ -9,6 +14,19 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({message: "123"})
+})
+
+app.get('/get-message', (req, res) => {
+    emitter.once('newMessage', (message) => {
+        res.json(message)
+    })
+})
+
+app.post('/new-message', (req, res) => {
+    const message = req.body;
+    emitter.emit('newMessage', message);
+    res.status(200);
+
 })
 
 app.listen(PORT, () => {
