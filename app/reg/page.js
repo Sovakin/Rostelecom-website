f29d8ad10abf0b6b54ce54e9beaea4eb6c9dc429
JAVE232323 <a.tyulmenev@yandex.ru>
2024-10-16 04:32:25 +0300

    cwqe

---
+++ app/reg/page.js
@@ -0,0 +1,11 @@
+import LoginForm from "../components/LoginForm";
+import RegisterForm from "../components/RegistrationForm";
+
+export default function Login() {
+    return (
+        <div className="login-page">
+            <h1>Регистрация</h1>
+            <RegisterForm />
+        </div>
+    );
+}
